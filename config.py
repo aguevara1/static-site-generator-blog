0b9e7afd8fd403cab6b0d30f84618631c50cb275
Alberto <albguevara12@gmail.com>
2025-03-24 00:47:19 -0500

    update in march

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ BASE_URLS = {
     "production": "alldev.click",
 }
 
-SITE_ENV = os.environ.get("SITE_ENV", "local")
+SITE_ENV = os.environ.get("SITE_ENV", "production")
 BASE_URL = BASE_URLS[SITE_ENV]
 ROOT_DIR = "pages"
 LAYOUTS_BASE_DIR = "_layouts"
